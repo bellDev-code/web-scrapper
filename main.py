a19be446b19c878c016a65d57fd8a3a25ce87ed3
bellDev-code <sumaoo20@naver.com>
2023-07-30 22:00:59 +0900

    extract_wwr_jobs function & refactoring

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from requests import get
-from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
 
-base_Url = 'https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term='
-search_term = 'python'
-
-response = get(f"{base_Url}{search_term}")
-if response.status_code != 200:
-    print("Can't request website")
-else:
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all('section', class_="jobs")
-    for job_section in jobs:
-        job_posts = job_section.find_all('li')
-        job_posts.pop(-1)
-        for posts in job_posts:
-            print(posts)
-            print("//////////")
+jobs = extract_wwr_jobs("python")
+print(jobs)
