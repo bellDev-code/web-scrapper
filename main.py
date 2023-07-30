3bd824c1f73c68ae1fedc5757f0174ead47a8c90
bellDev-code <sumaoo20@naver.com>
2023-07-31 00:19:46 +0900

    selenium & chrome brower & recursive

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+chrome_binary_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+chrome_options.binary_location = chrome_binary_path
+
+browser = webdriver.Chrome(service=Service(), options=chrome_options)
+
+base_url = 'https://kr.indeed.com/jobs?q='
+search_term = 'python'
+
+browser.get('https://kr.indeed.com/jobs?q=python&limit=50')
+
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find("ul", class_='jobsearch-ResultsList')
+jobs = job_list.find_all('li', recursive=False)
+for job in jobs:
+    print(job)
+    print("/////")
