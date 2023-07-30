035737a6f1da063b9dee448a9ff07cdf41de1fb4
bellDev-code <sumaoo20@naver.com>
2023-07-30 22:00:23 +0900

    list 할당 방법

--- text.py
+++ text.py
@@ -1,6 +1,15 @@
+"""
 def say_hello(name, age):
     print(f"hello {name}, my age is {age} years old")
 
 say_hello("jongho", 12)
 # 순서를 상관없이 arg 할당해도 된다.
 say_hello(age=13, name="youngdo")
+"""
+
+lits_of_numbers = [1, 2, 3]
+
+# 단, lists의 길이를 알고 있을때만 사용할 수 있다.
+first, second, third = lits_of_numbers
+
+print(first, second, third)
