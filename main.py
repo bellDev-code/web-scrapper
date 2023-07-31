c3a6c77d44a397ea1ec0791d9e12d71e54f6e3d4
bellDev-code <sumaoo20@naver.com>
2023-07-31 15:44:07 +0900

    Function extract_indeed_jobs & Function get_page_count

--- main.py
+++ main.py
@@ -10,14 +10,42 @@ chrome_options.binary_location = chrome_binary_path
 
 browser = webdriver.Chrome(service=Service(), options=chrome_options)
 
-base_url = 'https://kr.indeed.com/jobs?q='
-search_term = 'python'
+def get_page_count(keyword):
+    base_url = 'https://kr.indeed.com/jobs?q='
+    browser.get(f"{base_url}{keyword}")
+    
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    pagination = soup.find('nav', class_="ecydgvn0")
+    if pagination == None:
+        return 1
+    pages = pagination.find_all('div', recursive=False)
+    print(len(pages))
 
-browser.get(f"{base_url}{search_term}")
+get_page_count("nest")
 
-soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find("ul", class_='jobsearch-ResultsList')
-jobs = job_list.find_all('li', recursive=False)
-for job in jobs:
-    print(job)
-    print("/////")
+def extract_indeed_jobs(keyword):
+    base_url = 'https://kr.indeed.com/jobs?q='
+    browser.get(f"{base_url}{keyword}")
+    
+    results = []
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    job_list = soup.find("ul", class_='jobsearch-ResultsList')
+    jobs = job_list.find_all('li', recursive=False)
+    for job in jobs:
+        zone = job.find("div", class_="mosaic-zone")
+        if zone == None:
+            anchor = job.select_one("h2 a")
+            title = anchor['aria-label']
+            link = anchor['href']
+            name = job.find("span", class_= "companyName")
+            location = job.find("div", class_="companyLocation")
+            job_data = {
+                'link': f"https://kr.indeed.com{link}",
+                'company': name.string,
+                'location': location.string,
+                'position': title,
+            }
+            results.append(job_data)      
+    for result in results:
+        print(result, "/////\n///////")
+        
